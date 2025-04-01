3911c37ea602ec073b835fd2fd30aae5e280f94e
paras-verma7454 <parasverma7454@gmail.com>
2025-04-02 03:31:15 +0530

    FIX: try catch block

--- src/app/api/process-meeting/route.ts
+++ src/app/api/process-meeting/route.ts
@@ -4,25 +4,24 @@ import { z } from "zod";
 import { processMeeting } from "~/lib/assemblyAi";
 import { db } from "~/server/db";
 
-
 const bodyParser = z.object({
     meetingUrl: z.string(),
     projectId: z.string(),
     meetingId: z.string(),
-})
+});
 
-export const maxDuration = 300
+export const maxDuration = 300;
 
-export async function POST(req: NextRequest){
-    const {userId} = await auth()
-    if(!userId) return NextResponse.json({error: 'Unauthorized'}, {status: 401})
+export async function POST(req: NextRequest) {
+    const { userId } = await auth();
+    if (!userId) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
-    try{
-        const body = await req.json()
-        const {meetingUrl, projectId, meetingId} = bodyParser.parse(body)
-        const {summaries}= await processMeeting(meetingUrl)
+    try {
+        const body = await req.json();
+        const { meetingUrl, projectId, meetingId } = bodyParser.parse(body);
+        const { summaries } = await processMeeting(meetingUrl);
         await db.issue.createMany({
-            data: summaries.map(summary=>({
+            data: summaries.map(summary => ({
                 start: summary.start,
                 end: summary.end,
                 gist: summary.gist,
@@ -30,20 +29,21 @@ export async function POST(req: NextRequest){
                 summary: summary.summary,
                 meetingId,
             })),
-        })
+        });
 
         await db.meeting.update({
-            where:{
-                id: meetingId
+            where: {
+                id: meetingId,
             },
-            data:{
+            data: {
                 status: 'COMPLETED',
-                name: summaries[0]!.headline,
-            }
-        })
+                name: summaries[0] ? summaries[0].headline : 'Default',
+            },
+        });
 
-        return NextResponse.json({success: true},{status: 200})
-    }catch(err){
-
+        return NextResponse.json({ success: true }, { status: 200 });
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
 }
